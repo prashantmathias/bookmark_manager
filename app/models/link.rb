eeea849c534b53154df01a4f629e42190af5c0c7
Robert Summers <rsummers02@gmail.com>
2016-07-26 13:18:16 +0100

    updated link.rb

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,1 +1,15 @@
 require 'data_mapper'
+require 'dm-migrations'
+require 'dm-postgres-adapter'
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, 'postgres://localhost/bookmark_manager_test')
+
+class Link
+  include DataMapper::Resource
+
+  property :id,     Serial
+  property :url,    String
+  property :title,  String
+
+end
