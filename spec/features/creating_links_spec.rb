bb06b52af90724c4468aa3fb793eb6213eafaac8
Robert Summers <rsummers02@gmail.com>
2016-07-26 16:48:58 +0100

    11_creating_links.md

---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,15 @@
+feature "creating links" do
+  scenario "can add a link" do
+    visit "/links/new"
+    fill_in("title", with: "Google")
+    fill_in("url", with: "http://www.google.com")
+    click_button("Submit")
+
+    expect(current_path).to eq '/links'
+
+    within 'ul#links' do
+      expect(page).to have_content("Google")
+    end
+  end
+
+end
