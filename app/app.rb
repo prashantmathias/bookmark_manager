1344f8c0cf765897b978708a2d814657b0c6c817
Nomi Vos <nvoswork@gmail.com>
2016-07-26 09:54:00 -0500

    10_creating_a_modular_sinatra_app.md

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,11 @@
 require 'sinatra/base'
+require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
+
   get '/links' do
-    'Hello BookmarkManager!'
+    @links = Link.all
+    erb :'links/index'
   end
 
   # start the server if ruby file executed directly
