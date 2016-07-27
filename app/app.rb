409c1f15be9eeeb4fae6903cb9f281d1c00f2551
prashantmathias <mathiasaudio@gmail.com>
2016-07-27 12:42:35 +0100

    trying heroku again

--- app/app.rb
+++ app/app.rb
@@ -9,19 +9,19 @@ class BookmarkManager < Sinatra::Base
     "hello world"
   end
 
-  # get '/links' do
-  #   @links = Link.all
-  #   erb:'links/index'
-  # end
-  #
-  # get '/links/new' do
-  #   erb:'links/new'
-  # end
-  #
-  # post '/links' do
-  #   Link.create(url: params[:new_link], title: params[:title] )
-  #   redirect '/links'
-  # end
+  get '/links' do
+    @links = Link.all
+    erb:'links/index'
+  end
+
+  get '/links/new' do
+    erb:'links/new'
+  end
+
+  post '/links' do
+    Link.create(url: params[:new_link], title: params[:title] )
+    redirect '/links'
+  end
 
 
   # start the server if ruby file executed directly
