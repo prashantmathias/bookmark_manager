7f7995ad63f260ee34ed75215ba0d866c0344204
Nomi Vos <nvoswork@gmail.com>
2016-07-28 10:24:04 -0500

    modify app.rb, tagging_links_spec.rb

--- app/app.rb
+++ app/app.rb
@@ -20,9 +20,12 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.new(url: params[:url], title: params[:title])
-    tag = Tag.first_or_create(name: params[:tags])
-    link.tags << tag
+    link = Link.create(url: params[:url], title: params[:title])
+    params[:tags].split.each do |tag|
+      #tag = Tag.first_or_create(name: params[:tags])
+      link.tags << Tag.create(name: tag)
+    end
+    #link.tags << tag
     link.save
     redirect to('/links')
   end
--- spec/features/tagging_links_spec.rb
+++ spec/features/tagging_links_spec.rb
@@ -2,12 +2,12 @@
 
 feature "tagging links" do
 
-  before(:each) do
-    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
-    Link.create(url: 'http://www.google.com', title: 'Google', tags: [Tag.first_or_create(name: 'search')])
-    Link.create(url: 'http://www.zombo.com', title: 'This is Zombocom', tags: [Tag.first_or_create(name: 'bubbles')])
-    Link.create(url: 'http://www.bubble-bobble.com', title: 'Bubble Bobble', tags: [Tag.first_or_create(name: 'bubbles')])
-  end
+  # before(:each) do
+  #   Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
+  #   Link.create(url: 'http://www.google.com', title: 'Google', tags: [Tag.first_or_create(name: 'search')])
+  #   Link.create(url: 'http://www.zombo.com', title: 'This is Zombocom', tags: [Tag.first_or_create(name: 'bubbles')])
+  #   Link.create(url: 'http://www.bubble-bobble.com', title: 'Bubble Bobble', tags: [Tag.first_or_create(name: 'bubbles')])
+  # end
 
   scenario "adds a tag to a link" do
     visit "/links/new"
@@ -31,5 +31,16 @@ feature "tagging links" do
      expect(page).to have_content('This is Zombocom')
      expect(page).to have_content('Bubble Bobble')
     end
+  end
+
+  scenario 'adds multiple tags to a link' do
+    visit "/links/new"
+    fill_in("title", with: "Makers Academy")
+    fill_in("url", with: "http://www.makersacademy.com")
+    fill_in("tags", with: 'education coding')
+    click_button("Submit")
+
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education', 'coding')
   end
 end
